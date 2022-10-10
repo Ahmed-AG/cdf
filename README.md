78971623d8b64370660f1d6974008ab63126094c
Mohanad Abugharbia <mgharbieh.01@gmail.com>
2022-10-10 10:21:39 +0200

    pull request merge conflict resolve

--- README.md
+++ README.md
@@ -3,20 +3,20 @@
 Cloud Deployment Framework (CDF) provides an automated, cloud native, and secure way to deploy cloud resources. 
 Based on JSON configuration files, CDF uses AWS CDK to automatically create Codepipeline Pipelines that clone repos, run secrutiy checks on code, and deploy resources. It will also build any needed docker containers for Codebuild Projects
 
-![alt text](https://github.com/Ahmed-AG/cdf/blob/v0-1/cdf.jpg?raw=true)
+![alt text](https://github.com/Ahmed-AG/cdf/blob/main/cdf.jpg?raw=true)
 
 ## Supported tools
 ### Deployment tools
 Infrastructure as Code Tools  | Link |
 --- | --- |
 Cloud Formation | https://aws.amazon.com/cloudformation/
-Terraform (Coming soon) | https://www.terraform.io
+Terraform | https://www.terraform.io
 
 ### Security testing tools
 Tools | Description | Link |
 --- | --- | --- |
 cfn_nag | The cfn-nag tool looks for patterns in CloudFormation templates that may indicate insecure infrastructure | https://github.com/stelligent/cfn_nag
-checkov (Coming soon) | Checkov uses a common command line interface to manage and analyze infrastructure as code (IaC) scan results across platforms such as Terraform, CloudFormation, Kubernetes, Helm, ARM Templates and Serverless framework | https://www.checkov.io
+checkov | Checkov uses a common command line interface to manage and analyze infrastructure as code (IaC) scan results across platforms such as Terraform, CloudFormation, Kubernetes, Helm, ARM Templates and Serverless framework | https://www.checkov.io
 Semgrep (Coming soon) | Static analysis at ludicrous speed Find bugs and enforce code standards | https://semgrep.dev
 
 ## Requirements
@@ -40,7 +40,7 @@ Sample config.json file:
 {
     "pipelines" :[
         {
-            "name" : "Production-Pipeline1",
+            "name" : "cfn-pipeline1",
             "provider" : "aws",
             "source" : {
                 "source_type" : "codecommit",
@@ -64,7 +64,7 @@ Sample config.json file:
                     "checkov"
                 ]
             }
-        }
+        },
     ]
 }
 ```
--- code/cdf.py
+++ code/cdf.py
@@ -1,10 +1,7 @@
+#!/usr/bin/env python3
 import json
-from typing import Dict
 import aws_cdk as cdk
-from code.build_codepipeline import build_codepipeline
-from code.data_types.pipeline import *
-from code.data_types.iam_policy import *
-from code.data_types.definitions import *
+from code.pipeline import build_pipeline
 from constructs import Construct
 from aws_cdk import (
     Duration,
@@ -15,99 +12,27 @@ from aws_cdk import (
 
 class cdf(Stack):
 
-    def __init__(self, config_file: str, definitions_file: str, **kwargs) -> None:
+    def __init__(self, config_file, definitions_file, **kwargs) -> None:
         super().__init__(**kwargs)
 
         app = cdk.App()
 
         config = self.import_json_file(config_file)
         definitions = self.import_json_file(definitions_file)
-        try:
-            validation = validate_definitions(definitions)
-            if validation:
-                raise Exception(f"Definitions configuration is not valid: {validation}")
-        except KeyError as e:
-            raise KeyError(f"Definitions configuration is missing key {e}") from e
-        definitions = Definitions.from_json(json.dumps(definitions))
 
         for pipeline in config['pipelines']:
-            # Validate every pipeline configuration
-            try:
-                validation = validate_pipeline_config(pipeline)
-                if validation:
-                    raise Exception(f"Pipeline configuration is not valid: {validation}")
-            except KeyError as e:
-                raise KeyError(f'Pipeline configuration is missing key {e}') from e
-            pipeline = Pipeline.from_json(json.dumps(pipeline))
-
-            # Create BuildSpec
-            buildspec = self.generate_buildspec(pipeline, definitions)
-            print(json.dumps(buildspec, indent=4))
-
-            # Create IAM Statement
-            iam_policy_file = pipeline.deployment.iam_policy_file
+            # Import Iam policy file
+            iam_policy_file = pipeline['deployment']['iam_policy_file']
             iam_policy = self.import_json_file(iam_policy_file)
-
-            try:
-                validation = validate_iam_policy(iam_policy)
-                if validation:
-                    raise Exception(f"Iam policy configuration is not valid: {validation}")
-            except KeyError as e:
-                raise KeyError(f'Iam Policy is missing key {e}') from e
-
-            iam_policy = Iam_Policy.from_json(json.dumps(iam_policy))
             
-            # Build Pipeline
-            build_codepipeline(app, "cdf-" + pipeline.name, pipeline, buildspec, iam_policy)
+            # Build a pipeline
+            build_pipeline(app, "cdf-" + pipeline['name'], pipeline, definitions, iam_policy)
         app.synth()
         
-    def import_json_file(self, file: str) -> object:
+    def import_json_file(self, file):
         # TODO: Verify conf.d/config.yaml
         # TODO: Run pre-checks such as validating Github creds
-        with open(file, "r") as file_object:
-            file_json = json.load(file_object)    
+        file_object = open(file)
+        file_json = json.load(file_object)
+
         return file_json
-
-    def generate_buildspec(self, pipeline: Pipeline, definitions: Definitions) -> Dict[str, any]:
-        # Initializations
-        install_stage = {"commands" : []}
-        pre_build_stage = {"commands" : []}
-        build_stage = {"commands" : []}
-        post_build_stage = {"commands" : []}
-
-        # Creating install, pre_build, and post_build stages        
-        for check in pipeline.deployment.checks:
-            try:
-                if definitions.checks[check].__str__:
-                    for command in definitions.checks[check].install:
-                        install_stage['commands'].append(command)
-                    for command in definitions.checks[check].pre_build:
-                        pre_build_stage['commands'].append(command)
-                    for command in definitions.checks[check].post_build:
-                        post_build_stage['commands'].append(command)
-            except:
-                print("Check: ", check, "is not defined")
-
-        # Creating build stage
-        try:
-            if definitions.deployment[pipeline.deployment.type].build.__str__:
-                for command in definitions.deployment[pipeline.deployment.type].build:
-                    build_stage['commands'].append(command)
-        except:
-            print("Deployment definitions error!")    
-
-        # Build phases JSON object
-        phases = {
-            "install"       : install_stage,
-            "pre_build"     : pre_build_stage,
-            "build"         : build_stage,
-            "post_build"    : post_build_stage
-            }
-
-        # Build final buildspec JSON object
-        buildspec = {
-                "version"   : "0.2",
-                "phases"    : phases
-                }
-        
-        return buildspec
